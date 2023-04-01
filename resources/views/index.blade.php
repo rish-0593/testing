56f254684a5d5f09c09162cbcc7dec8233fcf1a8
rish_593 <kumarrishabh050@gmail.com>
2023-04-01 14:20:24 +0530

    fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@
                                 </tr>
                             @empty
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" colspan="4">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center" colspan="4">
                                         No Data
                                     </th>
                                 </tr>
