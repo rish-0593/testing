d2eca837da5048e9eb438d177e9128bb6a8e0bb6
rish_593 <kumarrishabh050@gmail.com>
2023-04-01 14:12:53 +0530

    fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
-                                    Product name
+                                    Product Name
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     Color
@@ -45,6 +45,11 @@
                                     </td>
                                 </tr>
                             @empty
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" colspan="4">
+                                        No Data
+                                    </th>
+                                </tr>
                             @endforelse
                         </tbody>
                     </table>
